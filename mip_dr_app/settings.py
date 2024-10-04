2c60fdce256b7e59e64adfbb920214b54458555b
Alex Manning <alexander.manning@stfc.ac.uk>
2024-10-04 16:47:25 +0100

    Make templates their own python package.

--- mip_dr_app/settings.py
+++ mip_dr_app/settings.py
@@ -10,8 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
-from pathlib import Path
 import os
+from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     "mip_dr_app_docs",
     "mip_dr_app_search",
     "mip_dr_app_vocab",
+    "mip_dr_app_templates",
 ]
 
 MIDDLEWARE = [
